225f879b2cfa4aec12189874c702ca6ae5fb6f8b
begovic2 <begovic2@illinois.edu>
2017-04-29 09:54:42 -0500

    Solved Problem 50

--- Python/Solved/Page1/Problem50.py
+++ Python/Solved/Page1/Problem50.py
@@ -1,23 +1,24 @@
 import math
 
+primes = []
 
-def sum_helper(p_index, best_len, primes):
+
+def sum_helper(p_index, best_len):
     best = 1  # longest sum so far
     goal = primes[p_index]
 
-    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-    for i in range(0, p_index):
-        print (goal, p_index, i, primes[i], best)
+    for i in range(0, 5):
+        #print (goal, p_index, i, primes[i], best)
         if primes[i]*best < goal:
             try:
                 for j in range(i + best_len - 1, p_index):
                     summation = sum(primes[i:j])
-                    print (summation)
+                    #print (summation)
                     if summation > goal:
                         raise StopIteration
                     elif summation == goal and (j-i+1) > best:
                         best = (j-i)
-                        print(primes[i:j])
+                        #print(primes[i:j])
             except StopIteration:
                 a = 2
 
@@ -38,17 +39,20 @@ def primes_sieve(limit):
 
 
 if __name__ == '__main__':
-    primes = primes_sieve(100)
+    primes = primes_sieve(1000000)
     print(primes)
     length = len(primes)  # 78,498 for 1,000,000
 
     best_pair = (0, 0)  # this is the prime, and the length of it's sum, in that order
 
     for i in range(0, length):
-        curr = sum_helper(i, best_pair[1], primes)
+        curr = sum_helper(i, best_pair[1])
         if curr > best_pair[1]:
             print(best_pair, " changed into: ", (primes[i], curr))
             best_pair = (primes[i], curr)
 
 
-    print(best_pair)
+    print(best_pair[0])
+
+
+# SOLVED : 997651
