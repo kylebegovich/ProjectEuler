f825f30bde9b36fc0a81f6ea0400af207b309e81
begovic2 <begovic2@illinois.edu>
2017-05-06 11:27:31 -0500

    Progress on problem 32

--- Python/Progress/Problem32.py
+++ Python/Progress/Problem32.py
@@ -2,11 +2,34 @@ import math
 sup = 10000
 inf = 1000
 
+pandigital_set = {}
 
-def has_pandigital_set(i):
-    condition = True
-    if condition:
+
+def is_pandigital(multiplicand, multiplier, product):
+    temp_str = str(multiplicand) + str(multiplier) + str(product)
+
+    print(multiplicand, multiplier, product, temp_str)
+
+    if len(temp_str) != 9:
+        return False
+
+    if len(set(temp_str)) == 9:
         return True
+
+
+def has_pandigital_set(num):
+    if num in pandigital_set:
+        print("this shouldn't happen, but it did:", num)
+        return
+
+    square = int(math.sqrt(num))
+    for x in range(1, square):
+
+        factor = (0.0 + num) / (x + 0.0)
+        if factor.is_integer():
+            if is_pandigital(x, int(factor), num):
+                return True
+
     return False
 
 
@@ -16,6 +39,6 @@ if __name__ == '__main__':
 
     for i in range(inf, sup+1):
         if has_pandigital_set(i):
-            count += 1
+            count += i
 
     print(count)
