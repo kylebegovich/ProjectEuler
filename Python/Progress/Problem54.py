9e74d2754c3d52a5c1eadce8f509fb48e661a010
begovic2 <begovic2@illinois.edu>
2017-05-31 11:30:22 -0500

    Solved Problem 54

--- Python/Progress/Problem54.py
+++ Python/Progress/Problem54.py
@@ -6,11 +6,13 @@ def has_flush(a_hand):
         print("Issue, has_flush")
         return False
     else:
-        temp = a_hand[0][1]
-        if all(card[1] == temp for card in a_hand):
-            return True  # also return highest card
+        last = len(a_hand[0])-1
+        temp = a_hand[0][last]
+        for card in a_hand:
+            if not card[len(card)-1] == temp:
+                return False
         else:
-            return False
+            return int(a_hand[4][0:2])
 
 
 def has_straight(a_hand):
@@ -20,19 +22,8 @@ def has_straight(a_hand):
     else:
         ranks = []
         for card in a_hand:
-            ranks.append(card[0])
-        for i in range(0, 5):
-            r = ranks[i]
-            if r == "T":
-                ranks[i] = "10"
-            elif r == "J":
-                ranks[i] = "11"
-            elif r == "Q":
-                ranks[i] = "12"
-            elif r == "K":
-                ranks[i] = "13"
-            elif r == "A":
-                ranks[i] = "14"
+            ranks.append(card[0:2])
+
         ranks = list(map(int, ranks))
         ranks = sorted(ranks)
         temp = ranks[0]
@@ -41,7 +32,7 @@ def has_straight(a_hand):
                 return False
             else:
                 temp += 1
-        return True  # also return highest card
+        return int(ranks[4])  # also return highest card
 
 
 def has_four_of_kind(a_hand):
@@ -49,19 +40,20 @@ def has_four_of_kind(a_hand):
         print("Issue, has_four_of_kind")
         return False
     else:
+        # get the ranks only from the hand
         for i in range(0, 5):
-            a_hand[i] = a_hand[i][0]
+            a_hand[i] = a_hand[i][0:2]
         temp1 = a_hand[0]
         temp2 = a_hand[1]
         if all(rank == temp1 for rank in a_hand[2:5]) or all(rank == temp2 for rank in a_hand[2:5]):
-            return True  # also return rank of card
+            return int(a_hand[3])  # also return rank of card
         elif temp1 == temp2:
             c = 2
             for i in range(2, 5):
                 if a_hand[i] == temp1:
                     c += 1
             if c == 4:
-                return True  # also return rank of card
+                return int(a_hand[0])  # also return rank of card
         return False
 
 
@@ -71,22 +63,22 @@ def has_three_of_kind(a_hand):
         return False
     else:
         for i in range(0, 5):
-            a_hand[i] = a_hand[i][0]
+            a_hand[i] = a_hand[i][0:2]
 
         if a_hand[0] == a_hand[1] == a_hand[2]:
-            return True  # also return rank of card
+            return int(a_hand[0])  # also return rank of card
         elif a_hand[0] == a_hand[1] or a_hand[0] == a_hand[2]:
             for i in range(3, 5):
                 if a_hand[i] == a_hand[0]:
-                    return True  # also return rank of card
+                    return int(a_hand[0])  # also return rank of card
         elif a_hand[1] == a_hand[2]:
             for i in range(3, 5):
                 if a_hand[i] == a_hand[1]:
-                    return True  # also return rank of card
+                    return int(a_hand[1])  # also return rank of card
         elif a_hand[3] == a_hand[4]:
             for i in range(0, 3):
                 if a_hand[i] == a_hand[3]:
-                    return True  # also return rank of card
+                    return int(a_hand[3])  # also return rank of card
         else:
             return False
 
@@ -97,30 +89,22 @@ def has_a_pair(a_hand):
         return False
     else:
         for i in range(0, 5):
-            a_hand[i] = a_hand[i][0]
+            a_hand[i] = a_hand[i][0:2]
 
-        for i in range(0, 5):
-            r = a_hand[i]
-            if r == "T":
-                a_hand[i] = "10"
-            elif r == "J":
-                a_hand[i] = "11"
-            elif r == "Q":
-                a_hand[i] = "12"
-            elif r == "K":
-                a_hand[i] = "13"
-            elif r == "A":
-                a_hand[i] = "14"
-        ranks = list(map(int, a_hand))
-        ranks = sorted(ranks)
+        to_ret = -1
         one_way_flag = False
+        a_hand = sorted(a_hand)
         for i in range(1, 4):
             curr = a_hand[i]
-            if a_hand[i-1] == curr or a_hand[i+1] == curr and not one_way_flag:
-                one_way_flag = True
-            elif a_hand[i-1] == curr or a_hand[i+1] == curr:
-                return False  # This is a two-pair
-        return one_way_flag  # also return rank of card
+            if curr == a_hand[i-1] and curr == a_hand[i+1]:
+                do_nothing = 0
+            elif curr == a_hand[i-1] and i == 1:
+                return int(a_hand[i])
+            elif curr == a_hand[i+1]:
+                if i == 3 or (i == 2 and not a_hand[2] == a_hand[4]) or (i == 1 and not a_hand[1] == a_hand[3]):
+                    return int(a_hand[i])
+
+        return False
 
 
 def has_two_pair(a_hand):
@@ -129,30 +113,40 @@ def has_two_pair(a_hand):
         return False
     else:
         for i in range(0, 5):
-            a_hand[i] = a_hand[i][0]
+            a_hand[i] = a_hand[i][0:2]
 
-        for i in range(0, 5):
-            r = a_hand[i]
-            if r == "T":
-                a_hand[i] = "10"
-            elif r == "J":
-                a_hand[i] = "11"
-            elif r == "Q":
-                a_hand[i] = "12"
-            elif r == "K":
-                a_hand[i] = "13"
-            elif r == "A":
-                a_hand[i] = "14"
-        ranks = list(map(int, a_hand))
-        ranks = sorted(ranks)
+        to_ret = -1
         one_way_flag = False
         for i in range(1, 4):
             curr = a_hand[i]
             if a_hand[i-1] == curr or a_hand[i+1] == curr and not one_way_flag:
                 one_way_flag = True
+                to_ret = curr
             elif a_hand[i-1] == curr or a_hand[i+1] == curr:
-                return True  # also return rank of cards
+                return max(int(curr), int(to_ret))  # also return rank of cards
         return False
+
+
+def my_sort(hand):
+    if not len(hand) == 5:
+        print("Issue, my sort")
+        return 0
+    else:
+        for i in range(len(hand)):
+            if hand[i][0] == "T":
+                hand[i] = "10" + hand[i][1]
+            elif hand[i][0] == "J":
+                hand[i] = "11" + hand[i][1]
+            elif hand[i][0] == "Q":
+                hand[i] = "12" + hand[i][1]
+            elif hand[i][0] == "K":
+                hand[i] = "13" + hand[i][1]
+            elif hand[i][0] == "A":
+                hand[i] = "14" + hand[i][1]
+            else:
+                hand[i] = "0" + hand[i]
+
+    return sorted(hand)
 
 
 def determine_hand_strength(hand):
@@ -160,40 +154,53 @@ def determine_hand_strength(hand):
         print("Issue, determine best hand")
         return 0
     else:
-        hand = sorted(hand)
+        hand = my_sort(hand)
+        print(hand)
 
         flush = has_flush(hand)
         straight = has_straight(hand)
-
-        if flush and straight:
+        pow = 8
+        if flush > 0 and straight > 0:
             print("straight flush")
+            return 100**pow + straight
 
+        pow -= 1
         four = has_four_of_kind(hand)
-
-        if four:
+        if four > 0:
             print("four of a kind")
+            return 100**pow + four
 
         three = has_three_of_kind(hand)
         pair = has_a_pair(hand)
-
-        if three and pair:
+        pow -= 1
+        if three > 0 and pair > 0:
             print("full house")
-        elif flush:
+            return 100 ** pow + three
+        pow -= 1
+        if flush > 0:
             print("flush")
-        elif straight:
+            return 100 ** pow + flush
+        pow -= 1
+        if straight > 0:
             print("straight")
-        elif three:
+            return 100 ** pow + straight
+        pow -= 1
+        if three > 0:
             print ("three of a kind")
+            return 100 ** pow + three
 
         two_pair = has_two_pair(hand)
-
-        if two_pair:
+        pow -= 1
+        if two_pair > 0:
             print("two pair")
-        elif pair:
+            return 100 ** pow + two_pair
+        pow -= 1
+        if pair > 0:
             print("pair")
+            return 100 ** pow + pair
         else:
             print("high card")
-
+            return int(hand[4][0:2])
 
 
 def does_first_win(curr_hand):
@@ -206,9 +213,14 @@ def does_first_win(curr_hand):
 
         print(first_hand, second_hand)
 
-        if determine_hand_strength(first_hand) > determine_hand_strength(second_hand):
+        f = determine_hand_strength(first_hand)
+        s = determine_hand_strength(second_hand)
+        print(f, s)
+        if f > s:
+            print("first wins")
             return True
         else:
+            print("second wins")
             return False
 
 
@@ -219,8 +231,10 @@ if __name__ == '__main__':
     for line in hands:
         if does_first_win(line[0:29]):
             count += 1
-            print(line)
 
     print(count)
 
     hands.close()
+
+
+# SOLVED : 376
