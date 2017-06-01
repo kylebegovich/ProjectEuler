35e8243b96326f92754659b44b674f6ec63c552b
begovic2 <begovic2@illinois.edu>
2017-06-01 08:34:40 -0500

    Organizational stuff

--- Python/Progress/Problem100.py
+++ Python/Progress/Problem100.py
@@ -1,13 +1,14 @@
 import math
+from decimal import *
 
 start = 1000000000000
-goal = 1.0/2.0
+goal = 0.5
 
 
 def condition(blue, red, total):
     """ checks if the kinda odd condition described in the problem is met or not """
 
-    if (blue - 0.0)/(total - 0.0) * (blue - 1.0)/(total - 1) == goal:
+    if Decimal((blue/total) * ((blue - 1)/(total - 1))) == Decimal(1/2):
         print (blue, red, total, "W O A H D O O D")
         return True
     else:
@@ -16,16 +17,21 @@ def condition(blue, red, total):
 
 
 def need_total_increment(blue, total):
-    if (blue - 0.0)/(total - 0.0) * (blue - 1.0)/(total - 1) > goal:
+    if (blue - 0.0)/(total - 0.0) * (blue - 1.0)/(total - 1.0) > goal + 0.000000000001:
         return True
     else:
         return False
 
 
 if __name__ == '__main__':
-    total = start
+
+    getcontext().prec = 100
+
+    total = float(start)
     blue = math.floor((total + 0.0) * math.sqrt(0.5))
-    red = total - blue
+    red = float(total - blue)
+
+    print(Decimal(1/2), Decimal(0.5), Decimal(1/2)*Decimal(2))
 
     while True:
 
@@ -39,6 +45,5 @@ if __name__ == '__main__':
             total += 1
             blue = math.floor(total * math.sqrt(0.5))
             red = total - blue
-
 
     print(blue, red, total)
