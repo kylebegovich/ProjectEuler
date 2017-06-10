e043bf4655c0a7f7762c8dab1c741212c44d733f
begovic2 <begovic2@illinois.edu>
2017-06-10 12:26:20 -0500

    Progress on Problem 88

--- Python/Progress/Problem88.py
+++ Python/Progress/Problem88.py
@@ -1,4 +1,30 @@
 import math
+product_sum_nums = []
+
+
+def product(lst):
+    result = 1
+    for num in lst:
+        result *= num
+
+    return result
+
+
+def find_min_p_s_num(k):
+    sum_nums = []
+    product_nums = []
+    for i in range(k):
+        sum_nums.append(1)
+        product_nums.append(1)
+
+    while True:
+        temp = product(product_nums)
+        if sum(sum_nums) == temp:
+            return temp
+        else:
+            do = 1  # no-op
+            # increment
+
 
 if __name__ == '__main__':
     print("answer")
