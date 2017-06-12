53e07b8c204b1661c409f7d73775351b29c81050
begovic2 <begovic2@illinois.edu>
2017-06-11 22:48:49 -0500

    progress on Problem 88

--- Python/Progress/Problem88.py
+++ Python/Progress/Problem88.py
@@ -10,21 +10,33 @@ def product(lst):
     return result
 
 
+def increment(lst):
+    l = len(lst)
+    for i in range(l-1, -1, -1):
+        if not lst[i] == 9:
+            lst[i] += 1
+            return lst
+
+
 def find_min_p_s_num(k):
-    sum_nums = []
-    product_nums = []
+    curr_nums = []
     for i in range(k):
-        sum_nums.append(1)
-        product_nums.append(1)
+        curr_nums.append(1)
 
     while True:
-        temp = product(product_nums)
-        if sum(sum_nums) == temp:
+        temp = product(curr_nums)
+        if sum(curr_nums) == temp:
             return temp
         else:
-            do = 1  # no-op
-            # increment
+            curr_nums = increment(curr_nums)
 
 
 if __name__ == '__main__':
-    print("answer")
+    print([0, 1, 2], product([0, 1, 2]))
+    print([3, 1, 2], product([3, 1, 2]))
+    print([7, 5, 2], product([7, 5, 2]))
+
+    print()
+
+    for i in range(10):
+        print(find_min_p_s_num(i))
