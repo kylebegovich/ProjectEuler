f3d6535234b3d1032f935676608a1cd51b24fe11
begovic2 <begovic2@illinois.edu>
2017-07-25 10:05:44 -0500

    Trying to solve 99

---
+++ Python/Progress/Problem99.py
@@ -0,0 +1,25 @@
+import math
+
+
+def get_num_size(base, pow):
+    return 0
+
+
+if __name__ == '__main__':
+    pair_file = open("p099_base_exp.txt", 'r').read().split('\n')
+
+    print(len(pair_file), pair_file[577], pair_file[708])
+
+    maximum = (0, 0)
+    for i in range(len(pair_file)):
+
+        line = pair_file[i]
+        comma = line.find(',')
+        base = math.log(float(line[:comma]))
+        power = int(line[comma+1:-1])
+        value = base * power
+        #print(base, power, value, i)
+        if value > maximum[0]:
+            maximum = (value, i)
+
+    print(maximum)
