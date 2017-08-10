99b68c90c5d4357cfd582476b6532fa295ce2fbb
begovic2 <begovic2@illinois.edu>
2017-08-10 00:31:41 -0500

    Made Fizbuzz better

--- Python/Solved/Page1/Problem1.py
+++ Python/Solved/Page1/Problem1.py
@@ -1,16 +1,8 @@
 n = 1
 a = 3
 b = 5
-summation = 0
 maximum = 1000
 
-if __name__ == '__main__':
-    while n < 1000:
-        if (n % 3 == 0 or n % 5 == 0):
-            summation += n
-        n += 1
-
-    print(summation)
-
+print(sum([n for n in range(1, 1000) if n % a == 0 or n % b == 0]))
 
 # SOLVED : 233168
