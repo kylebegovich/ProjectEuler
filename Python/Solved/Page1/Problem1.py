409383f9fae50318f6dcde7732913a48bdd33f72
begovic2 <begovic2@illinois.edu>
2017-08-10 00:33:54 -0500

    Even better FizzBuzz

--- Python/Solved/Page1/Problem1.py
+++ Python/Solved/Page1/Problem1.py
@@ -1,8 +1,7 @@
-n = 1
 a = 3
 b = 5
 maximum = 1000
 
-print(sum([n for n in range(1, 1000) if n % a == 0 or n % b == 0]))
+print(sum([n for n in range(1, maximum) if n % a == 0 or n % b == 0]))
 
 # SOLVED : 233168
