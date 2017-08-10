c297e90e642c2448f876abea9f4c9d12b57c5271
Arshia Malkani <arshiam2@illinois.edu>
2017-08-09 22:49:35 -0700

    Update Problem1.py

--- Python/Solved/Page1/Problem1.py
+++ Python/Solved/Page1/Problem1.py
@@ -2,6 +2,6 @@ a = 3
 b = 5
 maximum = 1000
 
-print(sum(list(filter((lambda x: x % 3 == 0 or x % 5 == 0), range(1, 1000)))))
+print(sum(list(filter((lambda x: x % a == 0 or x % b == 0), range(1, 1000)))))
 
 # SOLVED : 233168
