9216f6b0b66b005eca1f8abeb91fff8e9d4c85bb
begovic2 <begovic2@illinois.edu>
2017-08-17 14:23:57 -0500

    Solved Problem 65

---
+++ Python/Solved/Page2/Problem65.py
@@ -0,0 +1,18 @@
+from Progress.Euler import strings_to_ints
+
+def contfrac_to_frac(seq):
+    num, den = 1, 0
+    for u in reversed(seq):
+        num, den = den + num*u, num
+    return num, den
+
+
+seq = [2 * (i+1) // 3 if i%3 == 2 else 1 for i in range(100)]
+seq[0] += 1
+n, d = contfrac_to_frac(seq)
+
+# sorry this looks gross, just type issues needing to be resolved
+print( sum( strings_to_ints( list( str(n) ))))
+
+
+# SOLVED : 272
