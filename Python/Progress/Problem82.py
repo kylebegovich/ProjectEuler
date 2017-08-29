ef7d4c0bab347ef4dd6cc8a298b0a61cf499f795
Kyle Begovich <begovichkyle@gmail.com>
2017-08-29 11:49:42 -0500

    Local progress on problem 82... might be solved? i can't check atm

--- Python/Progress/Problem82.py
+++ Python/Progress/Problem82.py
@@ -8,6 +8,26 @@ for line in matrix:
     for elem in line_arr:
         temp.append(int(elem))
     grid.append(temp)
-    print(temp)
 
 length = len(grid)
+
+for col in range(1, length):
+
+    for row in range(length):
+        if row == 0:  # don't check above
+            grid[row][col] += min([grid[row+1][col], grid[row][col-1]])
+        elif row == length-1:  # don't check below
+            grid[row][col] += min([grid[row-1][col], grid[row][col-1]])
+        elif 0 < row < length:  # check left, top, and bottom origin points
+            grid[row][col] += min([grid[row-1][col], grid[row+1][col], grid[row][col-1]])
+
+
+minimum = grid[length-1][length-1]
+for i in range(length):
+    if grid[i][length-1] < minimum:
+        minimum = grid[i][length-1]
+
+print(minimum)
+
+
+# SOLVED : 1432
