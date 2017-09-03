68269f3717544aa4a67c6e6a34c29b97645d3395
Kyle Begovich <begovichkyle@gmail.com>
2017-09-03 13:15:04 -0500

    Solved 82 and other fun stuff too

--- Python/Progress/Problem82.py
+++ Python/Progress/Problem82.py
@@ -10,24 +10,22 @@ for line in matrix:
     grid.append(temp)
 
 length = len(grid)
+sol = [0] * length
 
-for col in range(1, length):
-
-    for row in range(length):
-        if row == 0:  # don't check above
-            grid[row][col] += min([grid[row+1][col], grid[row][col-1]])
-        elif row == length-1:  # don't check below
-            grid[row][col] += min([grid[row-1][col], grid[row][col-1]])
-        elif 0 < row < length:  # check left, top, and bottom origin points
-            grid[row][col] += min([grid[row-1][col], grid[row+1][col], grid[row][col-1]])
-
-
-minimum = grid[length-1][length-1]
 for i in range(length):
-    if grid[i][length-1] < minimum:
-        minimum = grid[i][length-1]
+    sol[i] = grid[i][length-1]
 
-print(minimum)
+for i in range(length - 2, -1, -1):
+    sol[0] += grid[0][i]
+
+    for j in range(1, length):
+        sol[j] = min(sol[j-1] + grid[j][i], sol[j] + grid[j][i])
+
+    for j in range(length - 2, -1, -1):
+        sol[j] = min(sol[j], sol[j+1] + grid[j][i])
 
 
-# Sad reax, not yet solved
+print(min(sol))
+
+
+# SOLVED : 260324
