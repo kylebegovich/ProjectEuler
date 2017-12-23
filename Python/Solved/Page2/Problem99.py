dac9537223b7574f35e77df525c48818a06ac04c
Kyle Begovich <begovichkyle@gmail.com>
2017-12-23 17:14:11 -0600

    Solved 99, swag monee

--- Python/Solved/Page2/Problem99.py
+++ Python/Solved/Page2/Problem99.py
@@ -2,11 +2,11 @@ import math
 
 
 def get_num_size(base, pow):
-    return 0
+    return base ** pow
 
 
 if __name__ == '__main__':
-    pair_file = open("p099_base_exp.txt", 'r').read().split('\n')
+    pair_file = open("text_files/p099_base_exp.txt", 'r').read().split('\n')
 
     print(len(pair_file), pair_file[577], pair_file[708])
 
@@ -16,10 +16,13 @@ if __name__ == '__main__':
         line = pair_file[i]
         comma = line.find(',')
         base = math.log(float(line[:comma]))
-        power = int(line[comma+1:-1])
+        power = int(line[comma+1:])
         value = base * power
-        #print(base, power, value, i)
+        # print(base, power, value, i)
         if value > maximum[0]:
-            maximum = (value, i)
+            maximum = (value, i+1)
 
     print(maximum)
+
+
+# SOLVED : 709
