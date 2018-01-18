ae50f3f2d651639e1d55fe2893b6b1b843de855c
Kyle Begovich <begovichkyle@gmail.com>
2018-01-17 19:35:20 -0600

    Progress (kinda) on problem 80, still a bug I do not know...

--- Python/Progress/Problem80.py
+++ Python/Progress/Problem80.py
@@ -16,22 +16,23 @@ def get_squares(upper_bound):
 def sum_digits_irrat_sqrt(num):
 
     s = str(Decimal(num).sqrt())
+    print(s)
     str_list = list(s[s.find('.')+1:])
 
-    total = 0
-    for i in str_list:
-        total += int(i)
-
-    return total
+    return sum([int(c) for c in str_list], 0)
 
 
 def main(upper_bound):
 
+    upper_bound += 1
     total = 0
-    squares = get_squares(upper_bound+1)
-    for i in range(2, upper_bound):
+    squares = get_squares(upper_bound)
+
+    for i in range(1, upper_bound):
         if i not in squares:
             total += sum_digits_irrat_sqrt(i)
+        else:
+            print(i**0.5)
 
     return total
 
