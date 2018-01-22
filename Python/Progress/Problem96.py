9d29f44025fd9579525ceb04c1a49d0c3eb69c7d
Kyle Begovich <begovichkyle@gmail.com>
2018-01-21 23:44:22 -0600

    Progress on sudoku solver 96

--- Python/Progress/Problem96.py
+++ Python/Progress/Problem96.py
@@ -24,8 +24,52 @@ def read_from_file(file_name):
     return puzzles
 
 
+def is_puzzle_done(puzzle):
+    for row in puzzle:
+        for elem in row:
+            if elem[0] == 0:
+                return False
+
+    return True
+
+
+def crosshatch(puzzle):
+    return puzzle  # write algorithm
+
+
+def memorization(puzzle):
+    return puzzle  # write algorithm
+
+
+def solve(puzzle_list):
+    print("begin solving")
+    new_list = []
+    for puzzle in puzzle_list:
+        while not is_puzzle_done(puzzle):
+            puzzle = crosshatch(puzzle)
+            puzzle = memorization(puzzle)
+        print(puzzle)
+        new_list.append(puzzle)
+
+    return new_list
+
+
+def add_first_three_all_puzzles(puzzles):
+    summation = 0
+    for puzzle in puzzles:
+        first_digit = puzzle[0][0][0]
+        second_digit = puzzle[0][1][0]
+        third_digit = puzzle[0][2][0]
+        summation += (first_digit * 100) + (second_digit * 10) + third_digit
+
+    return summation
+
+
 def main():
     puzzles = read_from_file("p096_sudoku.txt")
+    solved = solve(puzzles)
+    summation = add_first_three_all_puzzles(solved)
+    return summation
 
 
-main()
+print(main())
