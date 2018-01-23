f65399becba03fe81cd5949e2667bc255166a1d8
Kyle Begovich <begovichkyle@gmail.com>
2018-01-23 12:42:48 -0600

    fixed memorization, some kind of infinite loop to investigate on puzzle 2

--- Python/Progress/Problem96.py
+++ Python/Progress/Problem96.py
@@ -48,6 +48,14 @@ def crosshatch(puzzle):
         e_index = elems.index(0)
         row[e_index][0] = missing_val
 
+    #for row in nice_puzzle(puzzle)[0]:
+        #print(row)
+    #print()
+    #for row in nice_puzzle(puzzle)[1]:
+        #print(row)
+    #print("row-col")
+    #print()
+
     #  Columns
     for i in range(9):
         elems = [puzzle[j][i][0] for j in range(9)]
@@ -57,6 +65,14 @@ def crosshatch(puzzle):
         missing_val = (list((all_1_to_9 - set(elems)) - {0}))[0]
         e_index = elems.index(0)
         puzzle[e_index][i][0] = missing_val
+
+    #for row in nice_puzzle(puzzle)[0]:
+        #print(row)
+    #print()
+    #for row in nice_puzzle(puzzle)[1]:
+        #print(row)
+    #print("col-box")
+    #print()
 
     #  Boxes
     for b in range(9):
@@ -72,6 +88,14 @@ def crosshatch(puzzle):
         r, c = (e_index // 3) * 3, (e_index % 3) * 3
         puzzle[r][c][0] = missing_val
 
+    #for row in nice_puzzle(puzzle)[0]:
+        #print(row)
+    #print()
+    #for row in nice_puzzle(puzzle)[1]:
+        #print(row)
+    #print("box-mem")
+    #print()
+
     return puzzle
 
 
@@ -82,12 +106,21 @@ def memorization(puzzle):
                 row_elems = [puzzle[r][i][0] for i in range(9)]
                 col_elems = [puzzle[i][c][0] for i in range(9)]
                 tr = (r // 3) * 3
-                tc = (c % 3) * 3
+                tc = (c // 3) * 3
                 box_elems = [puzzle[tr][tc][0], puzzle[tr][tc+1][0], puzzle[tr][tc+2][0],
                              puzzle[tr+1][tc][0], puzzle[tr+1][tc+1][0], puzzle[tr+1][tc+2][0],
                              puzzle[tr+2][tc][0], puzzle[tr+2][tc+1][0], puzzle[tr+2][tc+2][0]]
+
                 invalid_elems = set(row_elems + col_elems + box_elems) - {0}
                 possible_elems = all_1_to_9 - invalid_elems
+
+                #print(row_elems)
+                #print(col_elems)
+                #print(box_elems)
+                #print(invalid_elems)
+                #print(possible_elems)
+                #print(r, c, tr, tc)
+
                 if len(possible_elems) == 1:
                     puzzle[r][c][0] = possible_elems.pop()
                 else:
@@ -96,17 +129,39 @@ def memorization(puzzle):
     return puzzle
 
 
+def nice_puzzle(puzzle):
+    n1 = [[0 for i in range(9)] for i in range(9)]
+    n2 = [[0 for i in range(9)] for i in range(9)]
+    for row in range(9):
+        for col in range(9):
+            n1[row][col] = puzzle[row][col][0]
+            n2[row][col] = puzzle[row][col][1]
+
+    return n1, n2
+
+
 def solve(puzzle_list):
-    print("begin solving")
+    #print("begin solving")
     new_list = []
     count = 0
     for puzzle in puzzle_list:
         while not is_puzzle_done(puzzle):
+            #for row in nice_puzzle(puzzle)[0]:
+                #print(row)
+            #print()
+            #for row in nice_puzzle(puzzle)[1]:
+                #print(row)
+            #print("mem-row")
+            #print()
+
             puzzle = crosshatch(puzzle)
             puzzle = memorization(puzzle)
+
         count += 1
         print(count)
-        print(puzzle)
+        for row in nice_puzzle(puzzle)[0]:
+            print(row)
+        print()
         new_list.append(puzzle)
 
     return new_list
