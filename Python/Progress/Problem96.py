6403ac0514d6c4598a9fb363bd62ca1c43952062
Kyle Begovich <begovichkyle@gmail.com>
2018-02-18 12:24:58 -0600

    added some code I found on reddit, probably safe

--- Python/Progress/Problem96.py
+++ Python/Progress/Problem96.py
@@ -1,8 +1,7 @@
 import sys
 
+
 all_1_to_9 = set(range(1, 10))
-
-
 verbose = False
 
 
@@ -350,6 +349,43 @@ def add_first_three_all_puzzles(puzzles):
     return summation
 
 
+def read_from_file2(file_name):
+    matrix = open(file_name, 'r')
+
+    # init puzzles to [0, set()] for every cell in every puzzle
+    puzzles = [[[0 for k in range(9)] for j in range(9)] for i in range(50)]
+
+    # fill in the actual values
+    grid_index = -1
+    row_index = 9
+    for line in matrix:
+        if "Grid" in line:
+            row_index = -1
+            grid_index += 1
+        else:
+            col_index = 0
+            for e in [int(i) for i in list(line)[:-1]]:
+                puzzles[grid_index][row_index][col_index][0] = e
+                col_index += 1
+
+        row_index += 1
+
+    return puzzles
+
+
+# ----- from https://www.reddit.com/r/ProgrammerHumor/comments/7yecbo/my_conjecture_any_solvable_problem_can_be/ -------
+lambda grid: all(map(lambda x: len(x) == len(set(x)),grid + list(zip(*tuple(grid))) + [(lambda a: list(a[0]+a[1]+a[2]))(list(zip(*tuple(grid[3*y:3*y+3])))[3*x:3*x+3]) for x in range(3) for y in range(3)]))
+
+
+def main2():
+    puzzles = read_from_file("p096_sudoku.txt")
+    solved_list = []
+    for puzz in puzzles:
+        solved_list.append(grid(puzz))
+    summation = add_first_three_all_puzzles(solved_list)
+    return summation
+
+
 def main():
     puzzles = read_from_file("p096_sudoku.txt")
     solved = solve(puzzles)
@@ -361,4 +397,4 @@ def main():
 if len(sys.argv) > 1 and sys.argv[1] == "-v":
     verbose = True
 
-print(main())
+print(main2())
