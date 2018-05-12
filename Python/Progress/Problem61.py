111753fae16cd37fb205de4b36a0d3477ac24174
Kyle Begovich <begovichkyle@gmail.com>
2018-05-12 17:32:20 -0500

    progress on 61

--- Python/Progress/Problem61.py
+++ Python/Progress/Problem61.py
@@ -1,30 +1,49 @@
 
-
 def triag(n):
     return (n * (n + 1)) / 2
-
 
 def square(n):
     return n * n
 
-
 def pent(n):
     return (n * (3*n - 1)) / 2
-
 
 def hexa(n):
     return n * (2*n - 1)
 
-
 def hept(n):
     return (n * (5*n - 3)) / 2
-
 
 def octa(n):
     return n * (3*n - 2)
 
 
-def fill_lists():
+def generate_chain(curr_chain, curr_groups, master_list):
+    if curr_chain is None or curr_groups is None or master_list is None:
+        return 0
+    if len(curr_chain) == 6 and (curr_chain[0] // 100 == curr_chain[5] % 100):
+        # this is the correct chain
+        return sum(curr_chain)
+    if len(curr_chain) > 6:
+        return 0
+
+    last = curr_chain[-1]
+    for i in range(1, 6):
+        print(i, "\n", curr_chain, "\n", curr_groups, "\n", master_list[i], "\n\n")
+        if 0 == curr_groups[i]:
+            for elem in master_list[i]:
+                if elem // 100 == last % 100:
+                    next_chain = curr_chain.append(elem)
+                    next_groups = curr_groups
+                    next_groups[i] = 1
+                    possible = generate_chain(next_chain, next_groups, master_list)
+                    if possible != 0:
+                        return possible
+                else:
+                    print(elem, last)
+
+
+def main():
     triag_4s = set()
     square_4s = set()
     pent_4s = set()
@@ -40,74 +59,51 @@ def fill_lists():
     for i in range(175):
         t = square(i)
         if 999 < t < 10000:
-            triag_4s.add(int(t))
+            square_4s.add(int(t))
 
     for i in range(150):
         t = pent(i)
         if 999 < t < 10000:
-            triag_4s.add(int(t))
+            pent_4s.add(int(t))
 
     for i in range(120):
         t = hexa(i)
         if 999 < t < 10000:
-            triag_4s.add(int(t))
+            hexa_4s.add(int(t))
 
     for i in range(120):
         t = hept(i)
         if 999 < t < 10000:
-            triag_4s.add(int(t))
+            hept_4s.add(int(t))
 
     for i in range(100):
         t = octa(i)
         if 999 < t < 10000:
-            triag_4s.add(int(t))
+            octa_4s.add(int(t))
 
-    return triag_4s, square_4s, pent_4s, hexa_4s, hept_4s, octa_4s
+    print(triag_4s, len(triag_4s), "\n")
+    print(square_4s, len(square_4s), "\n")
+    print(pent_4s, len(pent_4s), "\n")
+    print(hexa_4s, len(hexa_4s), "\n")
+    print(hept_4s, len(hept_4s), "\n")
+    print(octa_4s, len(octa_4s), "\n")
 
-
-def generate_chain(curr_list, curr_markers, master_set, master_list):
-    if len(curr_list) == 6 and (curr_list[0] // 100) == (curr_list[-1] % 100):
-        return curr_list
-    canidates = [n for n in master_set if (n // 100) == (curr_list[-1] % 100) and n not in curr_list]
-
-    # the check for
-    for cand in canidates:
-        cand_marker = find_marker(cand, master_list)
-        if cand_marker not in curr_markers:
-            curr_list.append(cand)
-            curr_markers.append(cand_marker)
-            #print(curr_list)
-            #print(curr_markers)
-            return generate_chain(curr_list, curr_markers, master_set, master_list)
-
-    return []
+    master_list = list()
+    master_list.append(list(triag_4s))
+    master_list.append(list(square_4s))
+    master_list.append(list(pent_4s))
+    master_list.append(list(hexa_4s))
+    master_list.append(list(hept_4s))
+    master_list.append(list(octa_4s))
+    print(master_list, len(master_list), "\n\n")
 
 
-def find_marker(num, master_list):
-    for i in range(len(master_list)):
-        if num in master_list[i]:
-            return i
-
-    print("SOMETHING BROKE, SEND HELP")
-    return -1
-
-
-def main():
-
-    sets = fill_lists()
-    master_set = sets[0] | sets[1] | sets[2] | sets[3] | sets[4] | sets[5]
-    master_list = [sets[0], sets[1], sets[2], sets[3], sets[4], sets[5]]
-
-    print(master_set)
-
-    for elem in master_set:
-        if len(str(elem)) != 4:
-            print(elem)
-            return
-        chain = generate_chain([elem], [find_marker(elem, master_list)], master_set, master_list)
-        if len(chain) == 6:
-            print("FOUND A CHAIN")
-            return chain
+    # can do only traig because one of the elems would be a triag
+    for elem in triag_4s:
+        answer = generate_chain([elem], [1, 0, 0, 0, 0, 0], master_list)
+        if answer is not None and answer != 0:
+            return answer
+        print(elem, "is not in the chain")
 
 
 print(main())
