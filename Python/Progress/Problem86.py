3471682921c4849a9cb26ddcf2e75b6c916b21a7
Kyle Begovich <begovichkyle@gmail.com>
2018-05-23 18:37:03 -0500

    Solved 86, used optimization from reference (wh)

--- Python/Progress/Problem86.py
+++ Python/Progress/Problem86.py
@@ -9,12 +9,15 @@ def is_min_path_int(lst):
     return True if sqrt(lst[0]**2 + (lst[1] + lst[2])**2).is_integer() else False
 
 
+def is_int_path(l, wh):
+    return sqrt(wh * wh + l * l).is_integer()
+
+
 def new_boxes(m):
     boxes = []
     app = boxes.append
-    for w in range(1, m+1):
-        for h in range(w, m+1):
-            app([m, w, h])
+    for wh in range(1, m * 2):
+        app((m, wh))
 
     return boxes
 
@@ -29,6 +32,24 @@ def naive(goal):
             if is_min_path_int([b[0], b[1], b[2]]):
                 #print(path, b, count)
                 count += 1
+
+        m += 1
+
+    return m-1, count
+
+
+def main(goal):
+    m = 1
+    count = 0
+    while count < goal:
+        print(m)
+        boxes = new_boxes(m)
+        for b in boxes:  # sorted(boxes, key=min_path):
+            if is_int_path(b[0], b[1]):
+                if b[1] <= b[0]:
+                    count += b[1] // 2
+                else:
+                    count += 1 + b[0] - (b[1] + 1) // 2
 
         m += 1
 
@@ -55,5 +76,8 @@ def ref(target):
     return l, count
 
 
-#print(main(1000000))
-print(ref(1000000))
+print(main(1000000))
+#print(ref(1000000))
+
+
+# SOLVED : 1818
