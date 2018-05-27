6647c35031469ef77e50c0efeb919e896bcc3098
Kyle Begovich <begovichkyle@gmail.com>
2018-05-26 23:58:53 -0500

    94 progress

--- Python/Progress/Problem94.py
+++ Python/Progress/Problem94.py
@@ -3,8 +3,43 @@ L = 333333333   # 1,000,000,000 / 3
 
 #  Pell's equation nonsense goes here:
 #   https://www.mathblog.dk/project-euler-94-almost-equilateral-triangles/
+#   x_k   y_k\sqrt{n} = (x_1   y_1\sqrt{n})^k
 
 
+def refernce_sol():
+    x = 2
+    y = 1
+    limit = 1000000000
+    result = 0
+
+    while (True):
+        # b = a+1
+        a_times3 = 2 * x - 1
+        area_times3 = y * (x - 2)
+        if a_times3 > limit:
+            break
+
+        if a_times3 > 0 and area_times3 > 0 and a_times3 % 3 == 0 and area_times3 % 3 == 0:
+            a = a_times3 / 3
+            result += 3 * a + 1
+
+        # b = a - 1
+        a_times3 = 2 * x + 1
+        area_times3 = y * (x + 2)
+
+        if a_times3 > 0 and area_times3 > 0 and a_times3 % 3 == 0 and area_times3 % 3 == 0:
+            a = a_times3 / 3
+            result += 3 * a - 1
+
+        nextx = 2 * x + y * 3
+        nexty = y * 2 + x
+
+        x = nextx
+        y = nexty
+
+    return result
+
+"""
 def main():
     summation = 0
 
@@ -19,6 +54,7 @@ def main():
             print(side, (3 * side) + 1, temp2, "+")
 
     print(summation)
+"""
 
 
-print(main())
+print(refernce_sol())
