3ba357292247e99865756837d821ac0cd78b81f0
Kyle Begovich <begovichkyle@gmail.com>
2019-05-04 00:38:47 -0500

    stashing old changes, problem 112

--- Python/Solved/Page3+/Problem112.py
+++ Python/Solved/Page3+/Problem112.py
@@ -1,4 +1,4 @@
-
+import time
 
 def is_decreasing(list_num):
     if len(list_num) == 0 or len(list_num) == 1:
@@ -22,12 +22,7 @@ def is_bouncy(list_num):
     return False
 
 
-def test_vals():
-    print(123, is_bouncy([1, 2, 3]))
-    print(121, is_bouncy([1, 2, 1]))
-    print(123456789, is_bouncy([1, 2, 3, 4, 5, 6, 7, 8, 9]))
-    print(1234567899999, is_bouncy([1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9]))
-    print(12345678994, is_bouncy([1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 4]))
+
 
 
 def inc_num(num):
@@ -40,12 +35,6 @@ def inc_num(num):
 
     return inc_num(num[:-1]) + [0]
 
-
-def test_inc_num():
-    print([], inc_num([]))
-    print([9], inc_num([9]))
-    print([1,2,3], inc_num([1,2,3]))
-    print([1,2,9], inc_num([1,2,9]))
 
 
 def num_proportion_above(bound):
@@ -66,8 +55,22 @@ def num_proportion_above(bound):
 
     return total_count-1
 
+start = time.time()
+ans = num_proportion_above(0.99)
+end = time.time()
+print(ans, 'time elapsed:', end - start, 'seconds')
 
-print(num_proportion_above(0.99))
 
+def test_vals():
+    print(123, is_bouncy([1, 2, 3]))
+    print(121, is_bouncy([1, 2, 1]))
+    print(123456789, is_bouncy([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    print(1234567899999, is_bouncy([1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9]))
+    print(12345678994, is_bouncy([1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 4]))
 
+def test_inc_num():
+    print([], inc_num([]))
+    print([9], inc_num([9]))
+    print([1,2,3], inc_num([1,2,3]))
+    print([1,2,9], inc_num([1,2,9]))
 # SOLVED : 1587000
