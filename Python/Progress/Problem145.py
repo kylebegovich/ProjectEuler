76d6aa580ad8739501eb54b4c00eb39114d00ecf
Kyle Begovich <begovichkyle@gmail.com>
2020-05-20 00:05:09 -0500

    optimized brute force, still slow af tho :(

--- Python/Progress/Problem145.py
+++ Python/Progress/Problem145.py
@@ -5,17 +5,46 @@ def reverse(n_str):
     return n_str[::-1]
 
 
+
+def single_step(n, should_print):
+    n_str = [int(d) for d in str(n)]  # this is one of the two slow lines
+    if n_str[-1] == 0:
+        return False
+
+    n_rev = 0
+    multiplier = 1
+    for digit in n_str:
+        n_rev += digit * multiplier
+        multiplier *= 10
+
+    flag = True
+    n_sum = n + n_rev
+    n_sum_str = [int(d) for d in str(n_sum)]  # this is the other slow line
+    for i in range(len(n_sum_str)):
+        if n_sum_str[i] % 2 == 0:
+            flag = False
+            break
+
+    if flag:
+        if should_print:
+            print(n, n_sum, "true")
+        return True
+    else:
+        if should_print:
+            print(n, n_sum)
+        return False
+
+
 memo = set()
 
-
-def main(limit):
+def main(start, limit):
     count = 0
-    n = 1
+    n = start
 
     while n < limit:
         if n in memo:
             count += 1
-            print("caught memo", n)
+            # print("caught memo", n)
             n += 2
             continue
 
@@ -49,19 +78,58 @@ def main(limit):
             count += 1
             memo.add(n_rev)
             memo.add(n)
-            n += 2
             print(n, n_sum, "true")
-        else:
             n += 1
-            print(n, n_sum)
+        else:
+            # print(n, n_sum)
+            n += 1
 
     print(sorted(memo))
     print(count)
 
-# main(30)
-# main(1000)
-main(10000)
-# main(100000)
-# main(120000)
+
+def is_reversible(num):
+    if num % 10 == 0:
+        return False
+
+    rev = 0
+    while num > 0:
+        rev = rev * 10 + num % 10
+        num = num // 10
+
+    added = num + rev
+    while added > 0:
+        if (added % 10) % 2 == 0:
+            return False
+        added = added // 10
+
+    return True
+
+def optimized_brute(limit):
+    count = 0
+    n = 1
+    while n < limit:
+        if n % 10000 == 0:
+            print(n, count)
+        if is_reversible(n):
+            count += 1
+        n += 1
+
+    print(count)
+    return count
+
+
+# main(1, 30)
+# main(1, 1000) # = 120
+# main(1, 10000)
+# main(1000, 10000)
+# main(100000, 500000)
+# main(10000, 100000)  # there are 0 of these... interesting
+# main(1, 100000)
+# main(100000, 120000)
+# main(1000000, 1200000)  # there are 0 of these... interesting
+# main(10000000, 10100000)
 # print(10 ** 9)
-# main(10 ** 9)
+# main(1, 10 ** 9)
+
+print(optimized_brute(10 ** 9))
