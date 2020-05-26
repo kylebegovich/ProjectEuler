75f6b321da99dc6e0f3f5ce58626daeb20315c5b
Kyle Begovich <begovichkyle@gmail.com>
2020-05-26 18:13:59 -0500

    progress on 103

--- Python/Progress/Problem103.py
+++ Python/Progress/Problem103.py
@@ -1,4 +1,4 @@
-
+from itertools import combinations
 
 
 # function described in the problem
@@ -13,6 +13,15 @@ def near_optimum(n, prev_A):
 # for i in range(2, 7):
 #     A = near_optimum(i, A)
 #     print(i, A)
+
+
+def wiggle_solutions(arr, wiggle):
+    potential_arrays = []
+    for i in range(len(arr)):
+        potential_arrays.append([j + arr[i] for j in range(-1 * wiggle, wiggle + 1)])
+
+    return potential_arrays
+
 
 
 # can prob parameterize sB, sC, lB, lC, if need be
@@ -32,10 +41,59 @@ def check_special_condition(B, C):
         return False
 
 
+def check_all_subsets(A):
+    print("check_all_subsets")
+    subsets = set()
+
+    for i in range(2, 8):
+        subsets = subsets.union(set(combinations(A, i)))
+
+    for pair in combinations(subsets, 2):
+        if not check_special_condition(pair[0], pair[1]):
+            print(False, pair[0], pair[1])
+            return False
+
+    return True
+
+
 n6 = [11, 18, 19, 20, 22, 25]
 
-def wiggle_solutions(arr, wiggle):
-    potential_arrays = []
-    for i in range(len(arr)):
-        for j in range(-1 * wiggle, wiggle + 1):
-            print("oof")
+def main():
+    arrs = wiggle_solutions(near_optimum(7, n6), 2)
+
+    min_sum = 1000
+    min_arr = []
+    curr_sum_set = 0
+    for v0 in arrs[0]:
+        curr_sum_set += v0
+        for v1 in arrs[1]:
+            curr_sum_set += v1
+            for v2 in arrs[2]:
+                curr_sum_set += v2
+                for v3 in arrs[3]:
+                    curr_sum_set += v3
+                    for v4 in arrs[4]:
+                        curr_sum_set += v4
+                        for v5 in arrs[5]:
+                            curr_sum_set += v5
+                            for v6 in arrs[6]:
+                                curr_sum_set += v6
+                                if curr_sum_set <= min_sum:
+                                    if check_all_subsets([v0, v1, v2, v3, v4, v5, v6]):
+                                        min_sum = curr_sum_set
+                                        min_arr = [v0, v1, v2, v3, v4, v5, v6]
+
+                                curr_sum_set -= v6
+                            curr_sum_set -= v5
+                        curr_sum_set -= v4
+                    curr_sum_set -= v3
+                curr_sum_set -= v2
+            curr_sum_set -= v1
+        curr_sum_set -= v0
+
+    print("oof")
+
+    print(check_all_subsets(near_optimum(7, n6)))
+
+
+main()
